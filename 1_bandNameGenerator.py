97586f4046d9d07c3c64bb71533c71a29fee6513
rorManish09 <manishchaudhary4970@gmail.com>
2023-03-05 22:02:00 +0530

    Day1 python

---
+++ 1_bandNameGenerator.py
@@ -0,0 +1,10 @@
+# This is a simple project for creating band name
+#Based on concatination of string
+print("This is a Band Name Generator:")
+#Enter the name of your city
+city=input("Enter the name of your city:")
+#Enter any Pet name
+pet=input("Enter the pet name:")
+
+print("Your Band name could  be...")
+print(city+" "+pet)
