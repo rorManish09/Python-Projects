212798fa6c375aa888fea05a46019a00437b0762
Manish Chaudhari <122202605+rorManish09@users.noreply.github.com>
2023-03-12 11:18:39 +0530

    Add files via upload

---
+++ 4_RockPaperAndScissor.py
@@ -0,0 +1,53 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+
+import random
+images=[rock,paper,scissors]
+user_choice=int(input("What do you want to choose?Type 0 for rack or 1 for paper and 2 for scissors"))
+computer_choice=random.randint(0,2)
+
+if user_choice>=3 or user_choice<0:
+    print("You typed invalid number")
+else:    
+    print(images[int(user_choice)])
+    print("Computer chose:")
+    print(images[int(computer_choice)])
+
+   
+    if user_choice==0 and computer_choice==2:
+        print("You win")
+    elif computer_choice==0 and user_choice ==2:
+        print("You lose")
+    elif  user_choice>computer_choice:
+        print("You win")
+    elif user_choice<computer_choice:
+        print("You lose")
+    elif computer_choice==user_choice:
+        print("Its a draw!")
+
